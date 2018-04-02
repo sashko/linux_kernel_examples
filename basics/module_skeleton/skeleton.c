79676447caea2d41788b184603fdd8894fd76edd
Oleksandr Kravchuk <dev@sashko.rv.ua>
2018-04-02 22:34:20 +0200

    module_skeleton: fix formatting

--- basics/module_skeleton/skeleton.c
+++ basics/module_skeleton/skeleton.c
@@ -1,5 +1,5 @@
-#include <linux/module.h>
 #include <linux/init.h>
+#include <linux/module.h>
 
 MODULE_AUTHOR("Oleksandr Kravchuk");
 MODULE_LICENSE("WTFPL");
@@ -10,14 +10,14 @@ MODULE_DESCRIPTION("Linux kernel module skeleton");
 
 static int __init skeleton_init(void)
 {
-        printk(KERN_DEBUG DRV_NAME ": %s()\n", __FUNCTION__);
+	printk(KERN_DEBUG DRV_NAME ": %s()\n", __FUNCTION__);
 
-        return 0;
+	return 0;
 }
 
 static void __exit skeleton_exit(void)
 {
-        printk(KERN_DEBUG DRV_NAME ": %s()\n", __FUNCTION__);
+	printk(KERN_DEBUG DRV_NAME ": %s()\n", __FUNCTION__);
 }
 
 module_init(skeleton_init);
