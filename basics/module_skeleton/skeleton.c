a8a4daf7416135a127aa7f5cdf6f2ba21f929164
Oleksandr Kravchuk <dev@sashko.rv.ua>
2018-04-09 02:28:53 +0200

    module_skeleton: use pr_info instead of pr_debug

--- basics/module_skeleton/skeleton.c
+++ basics/module_skeleton/skeleton.c
@@ -1,25 +1,24 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-MODULE_AUTHOR("Oleksandr Kravchuk");
-MODULE_LICENSE("Dual MIT/GPL");
-MODULE_DESCRIPTION("Linux kernel module skeleton");
-
 #define DRV_NAME "skeleton"
 
 
 static int __init skeleton_init(void)
 {
-	pr_debug("%s: %s\n", DRV_NAME, __func__);
+	pr_info("%s: %s\n", DRV_NAME, __func__);
 
 	return 0;
 }
 
 static void __exit skeleton_exit(void)
 {
-	pr_debug("%s: %s\n", DRV_NAME, __func__);
+	pr_info("%s: %s\n", DRV_NAME, __func__);
 }
 
 module_init(skeleton_init);
 module_exit(skeleton_exit);
 
+MODULE_AUTHOR("Oleksandr Kravchuk");
+MODULE_DESCRIPTION("Linux kernel module skeleton");
+MODULE_LICENSE("Dual MIT/GPL");
